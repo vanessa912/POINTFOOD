03cc56ea56e9848bf32cd5ed7433e188d01db0ea
Vronic20 <52298805+Vronic20@users.noreply.github.com>
2020-06-19 09:16:25 -0500

    Add files via upload

--- src/main/java/pe/edu/upc/spring/repository/IMesaRepository.java
+++ src/main/java/pe/edu/upc/spring/repository/IMesaRepository.java
@@ -1,9 +1,11 @@
 package pe.edu.upc.spring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.spring.model.Mesa;
 
+@Repository
 public interface IMesaRepository extends JpaRepository <Mesa, Integer> {
 	
 
